f2bfb4dd7b25c5de7705ff196df96a82834be992
devtobi <28538704+devtobi@users.noreply.github.com>
2023-11-20 09:05:43 +0100

    :wrench: Added configuration for plugins

--- src/config.js
+++ src/config.js
@@ -3,8 +3,24 @@
  * @type {{singleAttributePerLine: boolean, trailingComma: string}}
  */
 const muenchenPrettierCodeFormat = {
+    $schema: "https://json.schemastore.org/prettierrc",
     trailingComma: "es5",
-    singleAttributePerLine: true
+    singleAttributePerLine: true,
+    plugins: ["@ianvs/prettier-plugin-sort-imports"],
+    importOrder: [
+        "<BUILTIN_MODULES>", // Node-Modules
+        "",
+        "<TYPES>", // Standard types
+        "",
+        "<THIRD_PARTY_MODULES>", // Libraries
+        "",
+        "<TYPES>^[.]", // Own defined types
+        "",
+        "^@(/.*)$", // Relative imports with @
+        "^[.]", // Relative imports with @ .
+    ],
+    importOrderTypeScriptVersion: "5.2.2",
+    importOrderParserPlugins: ["typescript"],
 }
 
 export default muenchenPrettierCodeFormat;
