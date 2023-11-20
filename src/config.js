497c0d33599bade221a822e819f296a56542537c
devtobi <28538704+devtobi@users.noreply.github.com>
2023-11-20 08:27:02 +0100

    :memo: Added small comment for documentation purposes

--- src/config.js
+++ src/config.js
@@ -1,3 +1,7 @@
+/**
+ * This configuration only overrides specific settings where desired configuration options differ from Prettier standards.
+ * @type {{singleAttributePerLine: boolean, trailingComma: string}}
+ */
 const muenchenPrettierCodeFormat = {
     trailingComma: "es5",
     singleAttributePerLine: true
